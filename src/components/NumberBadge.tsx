1c04bd1c0d56c4c6659f4280e6e940a1d5eb0049
Nazmul Hassan <nazmulnhb007@yahoo.com>
2026-04-21 21:17:52 +0600

    `🔧 update(badge):` updated umberBadge component

--- src/components/NumberBadge.tsx
+++ src/components/NumberBadge.tsx
@@ -2,45 +2,47 @@ import { ActionIcon, Badge, CopyButton, Flex } from '@mantine/core';
 import { IconCopy, IconCopyCheck } from '@tabler/icons-react';
 import { isWatchlisted } from '../utils/formatters';
 
-type NumberBadgeProps = {
+type Props = {
     number: string;
     watchlist: string[];
     onClick?: (value: string) => void;
+    copyButton?: boolean;
 };
 
-export function NumberBadge({ number, watchlist, onClick }: NumberBadgeProps) {
+export function NumberBadge({ number, watchlist, onClick, copyButton = true }: Props) {
     const watched = isWatchlisted(number, watchlist);
     const isClickable = Boolean(onClick && number);
 
     return (
-        <CopyButton timeout={2000} value={number}>
-            {({ copied, copy }) => {
-                return (
-                    <Flex align={'center'}>
-                        <Badge
-                            color={watched ? 'orange' : 'blue'}
-                            onClick={() => {
-                                if (isClickable) {
-                                    onClick?.(number);
-                                }
-                            }}
-                            radius="sm"
-                            style={{ cursor: isClickable ? 'pointer' : 'default' }}
-                            variant={watched ? 'filled' : 'light'}
-                        >
-                            {number || 'Unknown'}
-                        </Badge>
-
-                        <ActionIcon
-                            color={copied ? 'teal' : 'gray'}
-                            onClick={copy}
-                            variant="subtle"
-                        >
-                            {copied ? <IconCopyCheck size={12} /> : <IconCopy size={12} />}
-                        </ActionIcon>
-                    </Flex>
-                );
-            }}
-        </CopyButton>
+        <Flex align={'center'}>
+            <Badge
+                color={watched ? 'orange' : 'blue'}
+                onClick={() => {
+                    if (isClickable) {
+                        onClick?.(number);
+                    }
+                }}
+                radius="sm"
+                style={{ cursor: isClickable ? 'pointer' : 'default' }}
+                variant={watched ? 'filled' : 'light'}
+            >
+                {number || 'Unknown'}
+            </Badge>
+            {copyButton && (
+                <CopyButton timeout={2000} value={number}>
+                    {({ copied, copy }) => {
+                        return (
+                            <ActionIcon
+                                color={copied ? 'teal' : 'gray'}
+                                onClick={copy}
+                                variant="subtle"
+                            >
+                                {copied ? <IconCopyCheck size={12} /> : <IconCopy size={12} />}
+                            </ActionIcon>
+                        );
+                    }}
+                </CopyButton>
+            )}
+        </Flex>
     );
 }
